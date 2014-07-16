aca7b21d3156992b569fd339bd7a106f347a29fb
Marija Jovicevic <marija.jovicevic@diwanee.com>
2014-07-16 17:25:33 +0200

    code seetings

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+
+//just test for JavaScript
+// trigger by event
+$('a.reveal-link').trigger('click');
+$('a.close-reveal-modal').trigger('click');
