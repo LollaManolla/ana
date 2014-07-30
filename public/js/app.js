1bff4c221298490f6b88699a5ea1564c0722e123
Marija Jovicevic <marija.jovicevic@diwanee.com>
2014-07-30 15:58:41 +0200

    rtl slider

--- public/js/app.js
+++ public/js/app.js
@@ -10,16 +10,46 @@ $( ".header__mob-content .main-nav__search" ).click(function() {
 });
 
 
-//start owl carousel
+//rtl owl carousel
 $(document).ready(function() {
-	$(".b-scroll-3thumbs__list").owlCarousel({
-		items : 3,
-		itemsDesktop: [1199,3],
-		itemsTablet: [600,2], 
-		itemsMobile : [479,1],		
-		navigation: true,
-		navigationText: ["&nbsp;","&nbsp;"],
-		pagination: false
-
-	});
+ var $b3thumbsSlider = $('.b-scroll-3thumbs__list');
+ if ($b3thumbsSlider.length > 0) {
+   if ($('html').attr("dir") === "rtl") {
+     $('.item', $b3thumbsSlider).each(function(i, o) {
+       var $o = $(o);
+       $($o.parent()).prepend($o);
+     });
+     var owlAfterInit = function(sld) {
+       var owlB3thumbInterval0 = setInterval(function() {
+         if ($(sld).data('owlCarousel') !== 'undefined') {
+           $('.owl-wrapper-outer', $b3thumbsSlider).css("direction", "ltr");
+           $('.owl-wrapper-outer .owl-wrapper', $b3thumbsSlider).css("direction", "rtl");
+           $(sld).data('owlCarousel').jumpTo(1000);
+           clearInterval(owlB3thumbInterval0);
+         }
+       }, 100);
+     };
+   }
+   else {
+     owlAfterInit = function(sld) {
+     };
+   }
+   $b3thumbsSlider.owlCarousel({
+     items: 3,
+     navigation: true,
+					navigationText: ["&nbsp;","&nbsp;"],
+     pagination: false,
+     itemsDesktop: false,
+     itemsDesktopSmall: false,
+     itemsTablet: [600, 2],
+     itemsTabletSmall: false,
+     itemsMobile: [479, 1],
+     itemsCustom: false,
+     addClassActive: true,
+     afterInit: owlAfterInit,
+     afterMove: function(s) {
+     }
+   });
+ }
 });
+
