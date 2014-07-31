d0beca4e73324857bdc8cea5d3e5a78bc49bbb08
Marija Jovicevic <marija.jovicevic@diwanee.com>
2014-07-31 11:47:48 +0200

    js

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+//$(document).foundation();
 
 //show search on mobile
+/*
 $( ".header__mob-content .main-nav__search" ).click(function() {
   $( "#search-mobile" ).toggle( "slow", function() {
     // Animation complete.
   });
-});
+});*/
 
 
 //rtl owl carousel
